719b231f0283d9fff2f5b22b0b4a55b9ac876288
Andrew Brown <35706781+andrewmbrown@users.noreply.github.com>
2023-05-12 18:38:13 -0400

    Delete run.sh

--- scripts/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-small-alpha.yaml --save_model --save_tokenizer --dont_save_arguments  -w 9a591205f989c23e19e9886c2181ef695bdc57d2
-# python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-small-beta.yaml --save_model --save_tokenizer --dont_save_arguments  -w 9a591205f989c23e19e9886c2181ef695bdc57d2
-# python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-small-charlie.yaml --save_model --save_tokenizer --dont_save_arguments -w 9a591205f989c23e19e9886c2181ef695bdc57d2
-python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-small-delta.yaml --save_model --save_tokenizer --dont_save_arguments -w 9a591205f989c23e19e9886c2181ef695bdc57d2
-python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-small-echo.yaml --save_model --save_tokenizer --dont_save_arguments -w 9a591205f989c23e19e9886c2181ef695bdc57d2
