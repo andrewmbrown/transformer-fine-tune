3f853ddf5b2cb5a0bd57068bb88d2a9f10bba479
andrew brown <andrewbrown@andrews-MacBook-Pro.local>
2023-05-31 01:00:54 -0400

    uncomment beta run

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,5 +7,5 @@
 # python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-small-echo.yaml --save_model --save_tokenizer --dont_save_arguments --use_wandb
 # python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-medium-echo.yaml --save_model --save_tokenizer --dont_save_arguments --use_wandb
 # python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-large-echo.yaml --save_model --save_tokenizer --dont_save_arguments --use_wandb
-python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-xl-echo.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
-# python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-xl-beta.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
+# python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-xl-echo.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
+python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-xl-beta.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
