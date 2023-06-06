7a35506613ec656ab7a287b0115dfe90c0c75dc2
andrewmbrown <17amb@queensu.ca>
2023-06-06 04:25:26 +0000

    new run.sh with deepspeed commands for SLURM environments

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,7 +7,7 @@
 # python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-small-echo.yaml --save_model --save_tokenizer --dont_save_arguments --use_wandb
 # python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-medium-echo.yaml --save_model --save_tokenizer --dont_save_arguments --use_wandb
 # python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-large-echo.yaml --save_model --save_tokenizer --dont_save_arguments --use_wandb
-# python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-xl-echo.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
+python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-xl-echo.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
 # python ./scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-xl-beta.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
 deepspeed scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-small-echo.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
 deepspeed scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-medium-echo.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
