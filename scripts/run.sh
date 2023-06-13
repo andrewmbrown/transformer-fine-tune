7e4e46132c4aeb0276f38dc46ac2ff4a52fabfb4
andrewmbrown <17amb@queensu.ca>
2023-06-13 20:45:23 +0000

    new sweep with lower learning rates

--- scripts/run.sh
+++ scripts/run.sh
@@ -52,6 +52,6 @@
 # deepspeed scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-xl-echo.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
 
 deepspeed scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-small/gpt2-small-exp5-complex.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
-deepspeed scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-medium/gpt2-medium-exp7-complex.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
-deepspeed scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-large/gpt2-large-exp7-complex.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
-deepspeed scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-xl/gpt2-xl-exp7-complex.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
+# deepspeed scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-medium/gpt2-medium-exp7-complex.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
+# deepspeed scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-large/gpt2-large-exp7-complex.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
+# deepspeed scripts/gptTrainHFDS.py -c /home/ubuntu/transformer-fine-tune/configs/gpt2-xl/gpt2-xl-exp7-complex.yaml --save_model --save_tokenizer --dont_save_arguments --use_deepspeed --use_wandb
